f79efcb72e1d951cec6b242ec98d19cf80076ae7
Tiffany Peck <74836265+Grannyma@users.noreply.github.com>
2022-12-03 15:49:00 -0500

    Add files via upload

--- ACM.py
+++ ACM.py
@@ -1,23 +1,34 @@
 #By: Tiffany Peck and Noah Coley
-#Last Modified: 12/02/22
+#Last Modified: 12/03/22 3:35PM
 #
 #
 #Instructions
 #1. To run the file, use CMD or Anaconda(python test1.py) to run the file
 #2.
-import pandas as pd
-import numpy as np
 import test1
 from test1 import user #get user value from test1
 from test1 import passI #get pass value from test1
 from test1 import userList #get list value from test1
-datafile = pd.read_csv("datafile.csv") # to read csv file named
+from test1 import readInput #get input value from test1
+
 class ACM:
     def __init__(self,u,p,l):
         self.userInp = u
         self.passInp = p
         self.listing = l
+    def switch(read):
+        if read == 1:
+            return "You can become a web developer."
+        elif read == 2:
+            return "You can become a backend developer."
+        elif read == 3:
+            return "You can become a Data Scientist"
+        elif read == 4:
+            return "You can become a Blockchain developer."
+        elif read == 5:
+            return "You can become a mobile app developer"
 acm1 = ACM(user, passI, userList)
-print(acm1.userInp)
-print(acm1.passInp)
-print(acm1.listing)
+swtich(readInput)
+#print(acm1.userInp)
+#print(acm1.passInp)
+#print(acm1.listing)
